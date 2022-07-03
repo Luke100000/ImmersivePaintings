25cc366253ddb06873fafba11e06bce122cb2371
Luke100000 <jonas.peche@aon.at>
2022-07-03 22:50:01 +0200

    fixed forge

--- forge/src/main/java/immersive_paintings/forge/CommonForge.java
+++ forge/src/main/java/immersive_paintings/forge/CommonForge.java
@@ -1,18 +1,14 @@
 package immersive_paintings.forge;
 
 import immersive_paintings.*;
-import immersive_paintings.cobalt.network.NetworkHandler;
 import immersive_paintings.forge.cobalt.network.NetworkHandlerImpl;
 import immersive_paintings.forge.cobalt.registration.RegistrationImpl;
-import immersive_paintings.network.s2c.PaintingListMessage;
 import immersive_paintings.resources.PaintingsLoader;
 import immersive_paintings.resources.ServerPaintingManager;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -34,35 +30,7 @@ public final class CommonForge {
     }
 
     @SubscribeEvent
-    private void onAddReloadListener(AddReloadListenerEvent event) {
-        event.addListener(new PaintingsLoader());
-    }
-
-    @SubscribeEvent
-    public static void onPlayerLoggedInEvent(PlayerEvent.PlayerLoggedInEvent event) {
-        if (!event.getPlayer().world.isClient) {
-            NetworkHandler.sendToPlayer(new PaintingListMessage(), (ServerPlayerEntity)event.getPlayer());
-        }
-    }
-
-    @SubscribeEvent
     public static void onCreateEntityAttributes(EntityAttributeCreationEvent event) {
         RegistrationImpl.ENTITY_ATTRIBUTES.forEach((type, attributes) -> event.put(type, attributes.get().build()));
-    }
-
-    public static boolean firstLoad = true;
-
-    @SubscribeEvent
-    public static void onClientStart(TickEvent.ClientTickEvent event) {
-        //forge decided to be funny and won't trigger the client load event
-        if (firstLoad) {
-            ClientMain.postLoad();
-            firstLoad = false;
-        }
-    }
-
-    @SubscribeEvent
-    public static void onServerStart(ServerStartingEvent event) {
-        ServerPaintingManager.server = event.getServer();
     }
 }
--- forge/src/main/java/immersive_paintings/forge/EventBus.java
+++ forge/src/main/java/immersive_paintings/forge/EventBus.java
@@ -1,20 +1,57 @@
 package immersive_paintings.forge;
 
+import immersive_paintings.ClientMain;
 import immersive_paintings.Main;
+import immersive_paintings.cobalt.network.NetworkHandler;
 import immersive_paintings.network.LazyNetworkManager;
+import immersive_paintings.network.s2c.PaintingListMessage;
+import immersive_paintings.resources.PaintingsLoader;
+import immersive_paintings.resources.ServerPaintingManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventBus {
     @SubscribeEvent
-    public void onServerTick(TickEvent.ServerTickEvent event) {
+    public static void onServerTick(TickEvent.ServerTickEvent event) {
         LazyNetworkManager.tickServer();
     }
 
     @SubscribeEvent
-    public void onClientTick(TickEvent.ClientTickEvent event) {
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
         LazyNetworkManager.tickClient();
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLoggedInEvent(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!event.getPlayer().world.isClient) {
+            NetworkHandler.sendToPlayer(new PaintingListMessage(), (ServerPlayerEntity)event.getPlayer());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onServerStart(ServerStartingEvent event) {
+        ServerPaintingManager.server = event.getServer();
+    }
+
+    public static boolean firstLoad = true;
+
+    @SubscribeEvent
+    public static void onClientStart(TickEvent.ClientTickEvent event) {
+        //forge decided to be funny and won't trigger the client load event
+        if (firstLoad) {
+            ClientMain.postLoad();
+            firstLoad = false;
+        }
+    }
+
+    @SubscribeEvent
+    public static void onAddReloadListener(AddReloadListenerEvent event) {
+        event.addListener(new PaintingsLoader());
     }
 }
