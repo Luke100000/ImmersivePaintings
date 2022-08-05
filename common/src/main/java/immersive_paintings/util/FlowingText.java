7b0fc7b21c2e77ba26c2014b116773dcd3fe5a84
Luke100000 <jonas.peche@aon.at>
2022-08-05 20:19:44 +0200

    cleanup

--- common/src/main/java/immersive_paintings/util/FlowingText.java
+++ common/src/main/java/immersive_paintings/util/FlowingText.java
@@ -2,7 +2,6 @@ package immersive_paintings.util;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.MutableText;
-import net.minecraft.text.OrderedText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 
@@ -10,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public record FlowingText(List<OrderedText> lines, float scale) {
+public class FlowingText {
     public static List<Text> wrap(Text text, int maxWidth) {
         return MinecraftClient.getInstance().textRenderer.getTextHandler().wrapLines(text, maxWidth, Style.EMPTY).stream().map(line -> {
             MutableText compiled = Text.literal("");
