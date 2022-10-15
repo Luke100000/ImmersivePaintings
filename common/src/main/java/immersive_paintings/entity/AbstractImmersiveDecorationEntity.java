46524d64cea2a007c909fd7002b2606be052922c
Luke100000 <jonas.peche@aon.at>
2022-10-15 17:39:26 +0200

    backport

--- common/src/main/java/immersive_paintings/entity/AbstractImmersiveDecorationEntity.java
+++ common/src/main/java/immersive_paintings/entity/AbstractImmersiveDecorationEntity.java
@@ -136,11 +136,6 @@ public abstract class AbstractImmersiveDecorationEntity extends Entity {
 
     @Override
     public boolean collides() {
-        return Config.getInstance().paintingsHaveCollision;
-    }
-
-    @Override
-    public boolean canHit() {
         return true;
     }
 
