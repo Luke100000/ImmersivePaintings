b280c57a1214fd7347c9a000a5a0008c72bf246e
Luke100000 <jonas.peche@aon.at>
2022-10-15 15:36:20 +0200

    added ram cache config flag

--- common/src/main/java/immersive_paintings/Config.java
+++ common/src/main/java/immersive_paintings/Config.java
@@ -40,6 +40,8 @@ public final class Config implements Serializable {
     public int minPaintingResolution = 8;
     public int maxPaintingResolution = 256;
 
+    public boolean keepImagesInRAM = true;
+
     public int version = 0;
 
     public static File getConfigFile() {
