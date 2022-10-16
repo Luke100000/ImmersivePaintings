2daf0e0391edf5aac3e824ac426cd4596fdd5f96
Luke100000 <jonas.peche@aon.at>
2022-10-16 10:03:31 +0200

    fixed invalid import

--- forge/src/main/java/immersive_paintings/forge/EventBus.java
+++ forge/src/main/java/immersive_paintings/forge/EventBus.java
@@ -10,7 +10,6 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
