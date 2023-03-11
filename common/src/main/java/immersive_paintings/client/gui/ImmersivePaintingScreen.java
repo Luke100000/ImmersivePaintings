46867f45f6088997b2a3997a94c8772cd97af205
Luke100000 <jonas.peche@aon.at>
2023-03-11 21:39:49 +0100

    cleanup

--- common/src/main/java/immersive_paintings/client/gui/ImmersivePaintingScreen.java
+++ common/src/main/java/immersive_paintings/client/gui/ImmersivePaintingScreen.java
@@ -218,7 +218,7 @@ public class ImmersivePaintingScreen extends Screen {
                 b.add(Page.PLAYERS);
             }
             b.add(Page.NEW);
-            if (!(entity instanceof ImmersiveGraffitiEntity)) {
+            if (!isGraffiti()) {
                 b.add(Page.FRAME);
             }
 
