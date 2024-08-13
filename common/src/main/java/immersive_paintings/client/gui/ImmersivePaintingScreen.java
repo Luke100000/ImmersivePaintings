90eb66ad24601a93d0b9a24b3ea9b85e07c12b79
Luke100000 <jonas.peche@aon.at>
2024-08-13 15:24:24 +0200

    cleanup

--- common/src/main/java/immersive_paintings/client/gui/ImmersivePaintingScreen.java
+++ common/src/main/java/immersive_paintings/client/gui/ImmersivePaintingScreen.java
@@ -27,10 +27,7 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.apache.commons.io.FilenameUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.*;
@@ -584,7 +581,7 @@ public class ImmersivePaintingScreen extends Screen {
                         tooltip.add(Text.translatable("immersive_paintings.right_click_to_delete").formatted(Formatting.ITALIC).formatted(Formatting.GRAY));
                     }
 
-                    paintingWidgetList.add(addDrawableChild(new PaintingWidget(ClientPaintingManager.getPaintingTexture(identifier, Painting.Type.THUMBNAIL), (int) (width / 2 + (x - 3.5) * 48) - 24, height / 2 - 66 + y * 48, 46, 46,
+                    paintingWidgetList.add(addDrawableChild(new PaintingWidget(ClientPaintingManager.getPaintingTexture(identifier, Painting.Type.THUMBNAIL), (int) (width / 2.0 + (x - 3.5) * 48) - 24, height / 2 - 66 + y * 48, 46, 46,
                             sender -> {
                                 entity.setMotive(identifier);
                                 NetworkHandler.sendToServer(new PaintingModifyRequest(entity));
@@ -751,7 +748,7 @@ public class ImmersivePaintingScreen extends Screen {
             try {
                 stream = new FileInputStream(path);
             } catch (Exception e) {
-                e.printStackTrace();
+                Main.LOGGER.error(e);
             }
         }
 
@@ -763,7 +760,7 @@ public class ImmersivePaintingScreen extends Screen {
                 stream.close();
                 return nativeImage;
             } catch (IOException e) {
-                e.printStackTrace();
+                Main.LOGGER.error(e);
             }
         }
 
@@ -902,7 +899,7 @@ public class ImmersivePaintingScreen extends Screen {
             double d = Math.sqrt(currentImage.getWidth() * currentImage.getWidth() + currentImage.getHeight() * currentImage.getHeight());
             double dw = currentImage.getWidth() / d;
             double dh = currentImage.getHeight() / d;
-            for (float diagonal = 3.0f; diagonal < 6.0; diagonal += target) {
+            for (double diagonal = 3.0f; diagonal < 6.0; diagonal += target) {
                 int pw = (int) Math.ceil(dw * diagonal);
                 int ph = (int) Math.ceil(dh * diagonal);
                 double e = Math.abs(pw / (double) ph - target) * Math.sqrt(5 + width + height);
