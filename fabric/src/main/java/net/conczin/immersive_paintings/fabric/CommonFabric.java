2c7dd3a10dd5049421ef2dc2e9b623dd19e6cd38
Lua MacDougall <lua@foxgirl.dev>
2026-03-01 17:11:46 -0800

    Fix Fabric crash due to init order

--- fabric/src/main/java/net/conczin/immersive_paintings/fabric/CommonFabric.java
+++ fabric/src/main/java/net/conczin/immersive_paintings/fabric/CommonFabric.java
@@ -17,6 +17,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricTrackedDataRegistry;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Registry;
@@ -39,7 +40,7 @@ public final class CommonFabric implements ModInitializer {
 
         // Entities and Serializers
         registerHelper(BuiltInRegistries.ENTITY_TYPE, Entities::registerEntities);
-        Entities.registerEntitySerializers((name, serializer) -> EntityDataSerializers.registerSerializer(serializer));
+        Entities.registerEntitySerializers(FabricTrackedDataRegistry::register);
 
         // Items and Creative Tabs
         registerHelper(BuiltInRegistries.ITEM, Items::registerItems);
